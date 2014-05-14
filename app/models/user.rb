07f7b891d72a6e91ad231276e461d14f50a4b6ff
Matt Hudgins <m.hudgins@gmail.com>
2014-05-14 16:55:00 -0400

    Adding simple_forms added https://teamtreehouse.com/forum/class-formhorizontal-does-not-work-in-simplefrom-in-ruby-tutorial-create-dropdown-list suggestions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :statuses
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+  					:first_name, :last_name, :profile_name
 
 end
